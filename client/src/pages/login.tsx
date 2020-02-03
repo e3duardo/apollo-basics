4014fb5920748bb9babf64b084d9a83384c7fe24
Eduardo Santos <e3duardo@live.com>
2020-02-03 15:02:40 -0300

    Update data with useMutation

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LoginForm, Loading } from "../components";
+import AppoloClient from "apollo-client";
+import * as LoginTypes from "./__generated__/login";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const client: AppoloClient<any> = useApolloClient();
+  const [login, { loading, error }] = useMutation<
+    LoginTypes.login,
+    LoginTypes.loginVariables
+  >(LOGIN_USER, {
+    onCompleted({ login }) {
+      localStorage.setItem("token", login as string);
+      client.writeData({ data: { isLoggedIn: true } });
+    }
+  });
+
+  if (loading) return <Loading />;
+  if (error) return <p>An error ocurred</p>;
+  return <LoginForm login={login} />;
 }
